ca65cd11374d7a8a3a092db37e3c2618cc920aa1
Parditka Vadvirág <vadviragpar@gmail.com>
2023-07-21 11:02:04 +0200

    add comment

--- src/fun/contains.ts
+++ src/fun/contains.ts
@@ -1,3 +1,8 @@
+/**
+ * igazat ad vissza, ha az elem benne van az array-ban
+ * @param array ebben keresünk
+ * @param elem ezt keressük
+ */
 export function contains(array: string[], elem: string): boolean {
 	return array.indexOf(elem) != -1;
 }
