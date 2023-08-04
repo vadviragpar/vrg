a871d288f92edde2eff172839714d20e8906f11c
Parditka Vadvirág <vadviragpar@gmail.com>
2023-08-04 13:11:15 +0200

    refactor shuffle

--- src/fun/shuffle.ts
+++ src/fun/shuffle.ts
@@ -9,15 +9,15 @@ export function shuffle<T>(array: T[]): T[] {
 	// létrehozzuk az új arrayt amibe az eredményt tesszük
 	let result = [];
 	// itt fogjuk tárolni a maradék elemeket
-	let remaining = array;
+	let remaining = array.slice();
 	// annyiszor húzunk ahány elem van
-	for (let i = 0; i < array.length; i++) {
+	while (remaining.length > 0) {
 		// választunk egy véletlen sorszámot
 		let index = getRandomInt(remaining.length);
 		// kivesszük az elemet amihez a sorszám tartozik
 		let elem = remaining[index];
 		// kitöröljük az elemet a megmaradtak közül
-		remaining = remaining.filter((e) => e != elem);
+		remaining.splice(index, 1);
 		// hozzáadjuk az elemet az eredményhez
 		result.push(elem);
 	}
