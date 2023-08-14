b085ab8f55fd48aa9bd683230b59e953953b5489
Parditka Vadvirág <vadviragpar@gmail.com>
2023-08-14 10:30:21 +0200

    vue setup

---
+++ src/view/StudentsView.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="header">Tanulók listája</div>
+	<div class="list">
+		<div class="student">Uzumaki Naruto</div>
+		<div class="student">Terry Pratchett</div>
+		<div class="student">
+			Don Bastiano João Coimbra de la Coronilla y Azevedo
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.header {
+	background-color: #2e2adc;
+	color: white;
+}
+.student {
+	background-color: #ff862e;
+	color: black;
+	border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/view/TestView.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Diákok</h1>
+  <p><i>hétszűnyű</i> Kapanyányi<b> MONYÓK</b></p>
+  <ul>
+    <li>aaaarrgh</li>
+  </ul>
+  <ol>
+    <li>
+      abc
+    </li>
+    <li>
+      xyz
+    </li>
+    <li>
+      <ul>
+        <li>
+          foo
+        </li>
+      </ul>
+    </li>
+  </ol>
+  <h1>Cukor</h1>
+  <h5>Borsó</h5>
+  <table>
+    <tbody>
+      <tr>
+        <td>Lorem, ipsum dolor.</td>
+        <td>Labore, minima veniam!</td>
+        <td>Neque, quos ad.</td>
+      </tr>
+      <tr>
+        <td>Lorem, ipsum dolor.</td>
+        <td>Ea, sequi officia?</td>
+        <td>Ipsa, in obcaecati.</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
