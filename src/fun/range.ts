b604a26ab7ddb6c816fc8da30316078d3cd7e66c
Parditka Vadvirág <vadviragpar@gmail.com>
2023-08-20 12:47:47 +0200

    add comment

--- src/fun/range.ts
+++ src/fun/range.ts
@@ -1,3 +1,8 @@
+/**
+ * létrehoz egy n elemű arrayt amiben egész számok vannak 0-tól n-ig (n már nem)
+ * @param n az új array elemszáma
+ */
+
 export function range(n: number): number[] {
 	return [...Array(n).keys()];
 }
