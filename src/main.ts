8ad3c3070f3772589b0ee62cf0e7e6b0987ef3bb
Parditka Vadvirág <vadviragpar@gmail.com>
2023-09-02 13:34:21 +0200

    make pwa

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,10 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", () => {
+		navigator.serviceWorker.register("sw.js");
+	});
+}
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,43 @@
-import { fileURLToPath, URL } from "node:url";
-
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: [
+				"favicon.ico",
+				"apple-touch-icon-180x180.png",
+				"logo.svg",
+			],
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+			},
+			manifest: {
+				name: "Vadi random generátora",
+				short_name: "VRG",
+				description: "Csoportokba rendezi a diákokat",
+				theme_color: "#000000",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
+	base: "",
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
